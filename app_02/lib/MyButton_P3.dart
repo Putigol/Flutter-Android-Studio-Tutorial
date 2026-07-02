bb8d1e8ce6090a29ebd9af63bf902f91939d589d
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-02 09:39:04 +0700

    Handle Events: Lock the Button

---
+++ app_02/lib/MyButton_P3.dart
@@ -0,0 +1,71 @@
+import "package:flutter/material.dart";
+
+class MyButton_P3 extends StatelessWidget {
+  const MyButton_P3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //Trả về Scaffold - widget cung cấp bố cục Material Design cơ bản
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App 02'),
+        backgroundColor: Colors.blueAccent,
+        elevation: 4, //Đổ bóng
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("B1!");
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              print("B2!");
+            },
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+
+
+
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            /*
+              ElevatedButton là 1 button nổi với hiệu ứng đổ bóng,
+              thường được sử dụng cho các hành động chính trong ứng dụng.
+            */
+            //Khoá nút nhấn
+            ElevatedButton(
+              onPressed: null,
+              child: Text("ElevatedButton", style: TextStyle(fontSize: 24)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                //màu nội dung bên trong
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                ),
+
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                //Đổ bóng
+                elevation: 5,
+              ),
+            ),
+            SizedBox(height: 20),
+            // InkWell
+            // InkWell không phải là button,
+            // nhưng nó cho phép tạo hiệu ứng gợn sóng (ripple effect)
+            // khi nhấn vào bất kỳ widget nào.
+
+          ],
+        ),
+      ),
+
+
+    );
+  }
+}
