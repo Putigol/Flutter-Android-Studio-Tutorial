07c6a3f0f2dd955022ada652d24a192921336a66
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-02 08:55:47 +0700

    Customize Button

---
+++ app_02/lib/MyButton_P2.dart
@@ -0,0 +1,86 @@
+import "package:flutter/material.dart";
+
+class MyButton_P2 extends StatelessWidget {
+  const MyButton_P2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //Trả về Scaffold - widget cung cấp bố cục Material Design cơ bản
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App 02'),
+        backgroundColor: Colors.blueAccent,
+        elevation: 4, //Đổ bóng
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("B1!");
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              print("B2!");
+            },
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Pressed!");
+        },
+
+        child: const Icon(Icons.add_ic_call),
+      ),
+
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            /*
+              ElevatedButton là 1 button nổi với hiệu ứng đổ bóng,
+              thường được sử dụng cho các hành động chính trong ứng dụng.
+            */
+            ElevatedButton(
+              onPressed: () {
+                print("ElevatedButton");
+              },
+              child: Text("ElevatedButton", style: TextStyle(fontSize: 24)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                //màu nội dung bên trong
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                ),
+
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 15,
+                ),
+                //Đổ bóng
+                elevation: 5,
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
