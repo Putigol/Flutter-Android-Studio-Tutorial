086199d281430515e7a370afcc7752a657ab9045
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-02 17:44:19 +0700

    TextField P2, Event Handler in TextField

--- app_02/lib/MyTextField.dart
+++ app_02/lib/MyTextField.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
 class MyTextField extends StatelessWidget {
   const MyTextField({super.key});
@@ -39,6 +40,7 @@ class MyTextField extends StatelessWidget {
                Đây là thành phần thiết yếu trong hầu hết các ứng dụng, từ biểu mẫu đăng nhập, tìm kiếm,
                đến nhập liệu trong các ứng dụng phức tạp.
               */
+              //Họ tên
               TextField(
                 decoration: InputDecoration(
                   labelText: "Họ và tên",
@@ -47,10 +49,12 @@ class MyTextField extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
+
+              //Email
               TextField(
                 decoration: InputDecoration(
                   labelText: "Email",
-                  hintText: "Nhập email",
+                  hintText: "abc@gmail.com",
                   //Ghi chú hỗ trợ
                   helperText: "Email không được để trống",
                   prefixIcon: Icon(Icons.email),
@@ -60,12 +64,79 @@ class MyTextField extends StatelessWidget {
                   ),
                   filled: true,
                   fillColor: Colors.blue[200],
-
                 ),
                 //Hỗ trợ nhập dạng email
                 keyboardType: TextInputType.emailAddress,
               ),
-            ],
+              SizedBox(height: 20),
+
+              //SĐT
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Số điện thoại",
+                  hintText: "Nhập số điện thoại",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: Icon(Icons.phone),
+                  suffixIcon: Icon(Icons.clear),
+                  filled: true,
+                  fillColor: Colors.blue[200],
+                ),
+                //Hỗ trợ nhập dạng số điện thoại
+                keyboardType: TextInputType.phone,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                ],
+              ),
+              SizedBox(height: 20),
+
+              //Ngày sinh
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Ngày sinh",
+                  hintText: "Nhập ngày sinh",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.date_range),
+                  suffixIcon: Icon(Icons.clear),
+                  filled: true,
+                  fillColor: Colors.blue[200],
+                ),
+                keyboardType: TextInputType.datetime,
+              ),
+              SizedBox(height: 20),
+
+              //Mât khẩu
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Mật khẩu",
+                  hintText: "Nhập mật khẩu",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  filled: true,
+                  fillColor: Colors.blue[200],
+                ),
+                obscureText: true, //Ẩn mật khẩu
+                obscuringCharacter: "*", //Ký hiệu ẩn
+              ),
+              SizedBox(height: 20),
+
+              //Sự kiện trong 1 TextField
+              TextField(
+                onChanged: (value) {
+                  print("Đang nhập: $value");
+                },
+                onSubmitted: (value) {
+                  print("Đã nhập: $value");
+                },
+                decoration: InputDecoration(
+                  labelText: "Câu hỏi bí mật",
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.text,
+              ),
+            ], //children
           ),
         ),
       ),
